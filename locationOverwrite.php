21dbc2123cae58d3076b0415039d3505720fda4c
APEX-15 LITE <yezj98>
2020-12-09 17:18:32 +0800

    Location overwrite debug

--- locationOverwrite.php
+++ locationOverwrite.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../location.php';
+require_once '../db/location.php';
 
 $reponse = array ();
 
@@ -10,18 +10,38 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
 
     if (isset($_POST['id']) and isset ($_POST ['latitude']) and isset ($_POST['longitude'])) {
 
-        if ($location -> overWrite ($_POST['id'], $_POST['latitude'], $_POST['longitude'])) {
-            
-            $reponse ['message'] = "location update successfully";
-            $reponse ['error'] = false;
-            $reponse ['latitude'] = $_POST ['latitude'];
-            $reponse ['longitude'] = $_POST ['longitude'];
-        }
+        //If the data is not null then overwrite    
         
-        else  {
-            $reponse ['message'] = " Location update fail";
-            $reponse ['error'] = true;
-        }
+        $result = $location -> overWrite ($_POST['id'], $_POST['latitude'], $_POST['longitude']); 
+
+            if ($result == 0) {
+                
+                $reponse ['message'] = "location update successfully";
+                $reponse ['error'] = false;
+                $reponse ['latitude'] = $_POST ['latitude'];
+                $reponse ['longitude'] = $_POST ['longitude'];
+            }
+            
+            elseif ($result == 1) {
+
+                if ($location ->storeLocation ($_POST['id'], $_POST['latitude'], $_POST['longitude'])) {
+                    $reponse ['message'] = "Store location succesfully";
+                    $reponse ['error'] = false;
+                    $reponse ['latitude'] = $_POST ['latitude'];
+                    $reponse ['longitude'] = $_POST ['longitude'];
+                }
+                
+                else {
+                    $reponse ['error'] = true;
+                    $reponse ['message'] = "Fields are missing";
+                }
+            }
+
+            elseif ($result == 2) {
+                $reponse ['error'] = true;
+                $reponse ['message'] = 'error';
+            }
+
 
     }
     else {
